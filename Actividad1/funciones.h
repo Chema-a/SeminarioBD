11c2f9436ae2797457c955b3b8e397039f6c3313
Chema <jose.maria4980@alumnos.udg.mx>
2020-09-23 12:15:35 -0500

    Implementacion de prueba en funciones.h

---
+++ Actividad1/funciones.h
@@ -0,0 +1,13 @@
+#if !defined(FUNCIONES_H)
+#define FUNCIONES_H
+void costo_de_envio(int a){
+    if( a < 30)
+        cout << a*60 << endl;
+    else:
+        cout << a*100 << endl;
+    cout << "Puedes pagar en linea" << endl;
+    cout << "Que pez" << endl;
+}
+
+
+#endif // FUNCIONES_H
