7b28798022662e92976f8f5f40031012ac2aaf84
Chema <jose.maria4980@alumnos.udg.mx>
2020-09-23 12:08:46 -0500

    Se implemento la condicional en funciones.h

---
+++ Actividad1/funciones.h
@@ -0,0 +1,17 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+void costo_de_envio(int a){
+    if( a > 20)
+        cout << a*12 << endl;
+    else:
+        cout << a*30 << endl;
+    cout << "Puedes pagar en linea" << endl;
+    cout << "Que pez" << endl;
+}
+
+
+
+
+#endif // FUNCIONES_H
+
